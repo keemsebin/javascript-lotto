217e506c46ce58cd3640cd1bfc45cc8745abfa93
keemsebeen <sebin0580@gmail.com>
2025-02-23 22:52:24 +0900

    feat: LottoNumber class 생성

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,63 +1,50 @@
 import { ERROR } from "../constants/message.js";
 import { throwError } from "../utils/throwError.js";
-import { isEmpty, isNumber, isRange } from "../utils/validation.js";
+import LottoNumber from "./LottoNumber.js";
 
 class Lotto {
   #numbers = [];
-  static LOTTO_NUMBER = Object.freeze({
-    MIN: 1,
-    MAX: 45,
-    LENGTH: 6,
-  });
 
   constructor(numbers) {
-    this.#numbers = this.sortLottoNumber(numbers);
+    const lottoNumbers = numbers.map((num) => new LottoNumber(num));
+    this.#numbers = this.sortLottoNumber(lottoNumbers);
     this.validate(this.#numbers);
   }
 
   isDuplicate(numbers) {
-    if (new Set(numbers).size !== numbers.length) {
+    const values = numbers.map((num) => num.getValue());
+    if (new Set(values).size !== values.length) {
       throwError(ERROR.DUPLICATE);
     }
   }
 
   checkLength(numbers) {
-    if (numbers.length !== this.constructor.LOTTO_NUMBER.LENGTH) {
+    if (numbers.length !== LottoNumber.LOTTO_NUMBER.LENGTH) {
       throwError(ERROR.LENGTH);
     }
   }
 
   validate(numbers) {
-    numbers.map((number) => {
-      isEmpty(number);
-      isNumber(number);
-      isRange(
-        {
-          min: this.constructor.LOTTO_NUMBER.MIN,
-          max: this.constructor.LOTTO_NUMBER.MAX,
-        },
-        this.constructor.LOTTO_NUMBER.LENGTH
-      );
-    });
     this.isDuplicate(numbers);
     this.checkLength(numbers);
   }
 
   sortLottoNumber(numbers) {
-    return numbers.sort((a, b) => a - b);
+    return numbers.sort((a, b) => a.getValue() - b.getValue());
   }
 
   getLottoNumbers() {
-    return this.#numbers;
+    return this.#numbers.map((number) => number.getValue());
   }
 
   getIncludeSameNumbers(givenLottoNumber) {
-    return this.#numbers.filter((number) => givenLottoNumber.includes(number))
-      .length;
+    return this.getLottoNumbers().filter((number) =>
+      givenLottoNumber.includes(number)
+    ).length;
   }
 
   hasBonusNumber(bonusNumber) {
-    return this.#numbers.includes(bonusNumber);
+    return this.getLottoNumbers().includes(bonusNumber);
   }
 }
 

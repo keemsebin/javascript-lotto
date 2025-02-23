ca0dd8ff55806766f759183dc9085212549968f1
keemsebeen <sebin0580@gmail.com>
2025-02-23 15:59:31 +0900

    feat: 구매 금액을 입력하지 않은 경우 에러 발생 테스트 추가

--- __tests__/PriceValidator.test.js
+++ __tests__/PriceValidator.test.js
@@ -1,12 +1,20 @@
 import PriceValidator from "../src/validation/PriceValidator.js";
 
 describe("입력한 구매 금액에 대한 유효성 검사를 진행한다", () => {
-  test("1000원 단위가 아닌 경우 에러를 발생시킨다.", () => {
-    const price = 1200;
+  test("구매 금액을 입력하지 않은 경우 에러를 발생시킨다.", () => {
+    const price = "";
 
     const priceValidator = new PriceValidator();
 
-    expect(() => priceValidator.checkThousandUnit(price)).toThrow("[ERROR]");
+    expect(() => priceValidator.validatePrice(price)).toThrow("[ERROR]");
+  });
+
+  test("구매 금액이 숫자가 아닌 경우 에러를 발생시킨다.", () => {
+    const price = "금액";
+
+    const priceValidator = new PriceValidator();
+
+    expect(() => priceValidator.validatePrice(price)).toThrow("[ERROR]");
   });
 
   test.each([500, 1000000])(
@@ -17,4 +25,12 @@ describe("입력한 구매 금액에 대한 유효성 검사를 진행한다", (
       expect(() => priceValidator.validatePrice(price)).toThrow("[ERROR]");
     }
   );
+
+  test("구입 금액이 1000원으로 나눠떨어지지 않는 경우 에러를 발생시킨다.", () => {
+    const price = 1200;
+
+    const priceValidator = new PriceValidator();
+
+    expect(() => priceValidator.checkThousandUnit(price)).toThrow("[ERROR]");
+  });
 });

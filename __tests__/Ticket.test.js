3e0de2399bd47472f9f099687b34b083392c30c9
keemsebeen <sebin0580@gmail.com>
2025-02-23 20:30:56 +0900

    refactor: 입력받은 금액에 해당하는 갯수 구하는 test 폴더 이동

--- __tests__/Ticket.test.js
+++ __tests__/Ticket.test.js
@@ -1,4 +1,17 @@
+import { PRICE } from "../src/domain/Price.js";
 import Ticket from "../src/domain/Ticket.js";
+import { divideByUnit } from "../src/utils/count.js";
+
+test("입력받은 금액에 해당하는 개수를 구한다.", () => {
+  // given
+  const money = 1000;
+
+  // when
+  const result = divideByUnit(money, PRICE.UNIT);
+
+  // then
+  expect(result).toBe(1);
+});
 
 test("정해진 개수만큼 로또를 생성한다.", () => {
   // given

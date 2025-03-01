0c62b9005224aebed3bbdd20c1d54c098841a5ab
keemsebeen <sebin0580@gmail.com>
2025-03-02 00:06:38 +0900

    refactor: Table 컴포넌트 template 추가

--- step2-web/components/common/Table/Table.js
+++ step2-web/components/common/Table/Table.js
@@ -1,50 +1,65 @@
-class Table {
-  constructor({ headers, data, styles = {} }) {
-    this.headers = headers;
-    this.data = data;
-    this.styles = styles;
-    this.element = document.createElement("table");
+import Component from "../../../core/component";
+import { styleStr } from "../../../helper/style";
 
-    Object.entries(this.styles).forEach(([property, value]) => {
-      this.element.style[property] = value;
-    });
-
-    this.init();
+class Table extends Component {
+  constructor() {
+    super();
   }
 
-  init() {
-    const thead = document.createElement("thead");
-    const headerRow = document.createElement("tr");
-
-    this.headers.forEach((headerText) => {
-      const th = document.createElement("th");
-      th.classList.add("cell");
-      th.textContent = headerText;
-      headerRow.appendChild(th);
-    });
-
-    thead.appendChild(headerRow);
-    this.element.appendChild(thead);
-
-    const tbody = document.createElement("tbody");
-
-    this.data.forEach((rowData) => {
-      const row = document.createElement("tr");
-      rowData.forEach((cellData) => {
-        const cell = document.createElement("td");
-        cell.classList.add("cell");
-        cell.textContent = cellData;
-        row.appendChild(cell);
-      });
-
-      tbody.appendChild(row);
-    });
-
-    this.element.appendChild(tbody);
+  setDefaultProps() {
+    this.props = {
+      headers: [],
+      data: [],
+      styles: {},
+    };
   }
 
-  render() {
-    return this.element;
+  template() {
+    return (props) => {
+      if (props) this.setProps(props);
+
+      const { headers, data, styles } = this.props;
+
+      const headerHtml = headers
+        .map(
+          (header) => `<th class="cell p-8 text-center border-b">${header}</th>`
+        )
+        .join("");
+
+      const rowsHtml = data
+        .map(
+          (rowData) =>
+            `<tr>${rowData
+              .map(
+                (cellData) =>
+                  `<td class="cell p-8 text-center border-b">${cellData}</td>`
+              )
+              .join("")}
+            </tr>`
+        )
+        .join("");
+
+      return `
+        <table 
+          class="w-full" 
+          style="${styleStr(
+            styles
+          )} table-layout: auto; border-collapse: collapse;">
+          <thead>
+            <tr>${headerHtml}</tr>
+          </thead>
+          <tbody>
+            ${rowsHtml}
+          </tbody>
+        </table>
+      `;
+    };
+  }
+
+  render(props) {
+    if (props) this.setProps(props);
+    const templateFn = this.template();
+    return templateFn(this.props);
   }
 }
 

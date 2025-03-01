24001e361d57e361b216a5e2e3e6e1220ea0d095
keemsebeen <sebin0580@gmail.com>
2025-03-02 00:05:01 +0900

    refactor : Text 컴포넌트 template 추가

--- step2-web/components/common/Text/Text.js
+++ step2-web/components/common/Text/Text.js
@@ -1,31 +1,5 @@
-// class Text {
-//   constructor(content, { classList = [], styles = {} }) {
-//     this.element = document.createElement("p");
-//     this.element.textContent = content;
-//     this.applyClassList(classList);
-//     this.applyStyles(styles);
-//   }
-
 import Component from "../../../core/component";
-
-//   applyClassList(classList) {
-//     classList.forEach((className) => {
-//       this.element.classList.add(className);
-//     });
-//   }
-
-//   applyStyles(styles) {
-//     Object.keys(styles).forEach((key) => {
-//       this.element.style[key] = styles[key];
-//     });
-//   }
-
-//   render() {
-//     return this.element;
-//   }
-// }
-
-// export default Text;
+import { styleStr } from "../../../helper/style";
 
 class Text extends Component {
   initState() {
@@ -43,7 +17,7 @@ class Text extends Component {
     };
   }
 
-  template(props) {
+  template() {
     return (props) => {
       if (props) this.setProps(props);
 
@@ -52,7 +26,7 @@ class Text extends Component {
       return `
       <p 
         class="${classList.join(" ")}" 
-        style="${this.getInlineStyle(styles)}">
+        style="${styleStr(styles)}">
         ${content}
       </p>
     `;
@@ -60,12 +34,6 @@ class Text extends Component {
   }
 
   setEvent() {}
-
-  getInlineStyle(styles) {
-    return Object.entries(styles)
-      .map(([key, value]) => `${key}: ${value}`)
-      .join(";");
-  }
 
   render(props) {
     if (props) this.setProps(props);
